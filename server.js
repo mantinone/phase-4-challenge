7344472285625fca6e0b75bf934bc22b29dbd944
Max Antinone <mantinone@gmail.com>
2017-06-21 10:46:13 -0700

    Removes moment where it's not used

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 const database = require('./database')
-const moment = require('moment')
 const app = express()
 
 const index = require('./routes/index')
